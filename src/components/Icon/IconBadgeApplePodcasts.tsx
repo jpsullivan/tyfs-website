a73f41fdebdcf717283db41ad6bc5005eb1f7038
Josh Sullivan <jaysche.sullivan@gmail.com>
2021-11-03 22:12:10 -0400

    app badge svg fixes

--- src/components/Icon/IconBadgeApplePodcasts.tsx
+++ src/components/Icon/IconBadgeApplePodcasts.tsx
@@ -58,8 +58,8 @@ export const IconBadgeApplePodcasts = React.memo<JSX.IntrinsicElements['svg']>(
             </linearGradient>
             <path
               id="XMLID_662_"
-              fill-rule="evenodd"
-              clip-rule="evenodd"
+              fillRule="evenodd"
+              clipRule="evenodd"
               fill="url(#XMLID_2_)"
               d="M32.1821,29.2281
 			c-0.2446,0.6213-0.809,1.4961-1.6094,2.1123c-0.4621,0.3559-1.0138,0.6961-1.7716,0.9206
@@ -73,8 +73,8 @@ export const IconBadgeApplePodcasts = React.memo<JSX.IntrinsicElements['svg']>(
             />
             <path
               id="XMLID_660_"
-              fill-rule="evenodd"
-              clip-rule="evenodd"
+              fillRule="evenodd"
+              clipRule="evenodd"
               fill="none"
               d="M32.1821,29.2281
 			c-0.2446,0.6213-0.809,1.4961-1.6094,2.1123c-0.4621,0.3559-1.0138,0.6961-1.7716,0.9206
